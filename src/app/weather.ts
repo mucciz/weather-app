559b600ea22b2470401eae9c350f2d15ad730161
Mucciz <kenmucciz8@gmail.com>
2022-07-02 16:04:31 +0300

    Set up weather interface

--- src/app/weather.ts
+++ src/app/weather.ts
@@ -1,2 +1,18 @@
 export interface Weather {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  hourly: hourlyInfo;
+  hourly_units: hourlyUnitsInfo;
+}
+
+interface hourlyInfo {
+  time: string[];
+  temperature_2m: number[];
+}
+
+interface hourlyUnitsInfo {
+  temperature_2m: number[];
+  time: string;
 }
