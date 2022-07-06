3a9086e5a2df0a4b2df69f7aaa75016bec09e9c8
Mucciz <kenmucciz8@gmail.com>
2022-07-06 17:53:34 +0300

    Experimental interface

--- src/app/weather.ts
+++ src/app/weather.ts
@@ -1,18 +1,27 @@
-export interface Weather {
+export interface IWeather {
   latitude: number;
   longitude: number;
   generationtime_ms: number;
   utc_offset_seconds: number;
-  hourly: hourlyInfo;
-  hourly_units: hourlyUnitsInfo;
+  current_weather: iCurrentWeather;
+  hourly: IHourlyInfo;
+  hourly_units: IHourlyUnitsInfo;
 }
 
-interface hourlyInfo {
+interface IHourlyInfo {
   time: string[];
   temperature_2m: number[];
 }
 
-interface hourlyUnitsInfo {
+interface IHourlyUnitsInfo {
   temperature_2m: number[];
   time: string;
+}
+
+interface iCurrentWeather {
+  tempertaure: number,
+  windspeed: number,
+  winddirection: number, 
+  weathercode: number,
+  time: string
 }
